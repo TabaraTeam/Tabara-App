e21510bce38cf438c5407869199b011f238a4a1a
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2024-08-07 03:13:47 +0900

    fix : Stack Navigator , Modal 기능 추가 , 기본 레이아웃 수정 , OverLay로 설정되어 있던 스타일 삭제, ModalScreen 예시 함수 작성 , createDrawerNavigator 함수 선언 추가 , 스플래시화면 timeout 모션 관련 코드 일부 삭제

--- App.js
+++ App.js
@@ -1,35 +1,138 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Modal} from 'react-native';
 import {NavigationContainer, useNavigation} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import MapScreen from './pages/map/MapScreen';
-import NextScreen from './pages/next/NextScreen';
-import {Button} from 'react-native';
+import {
+  createNativeStackNavigator,
+  CardStyleInterpolators,
+} from '@react-navigation/native-stack';
+import {Button, TouchableOpacity} from 'react-native';
+import SplashScreens from './Screens/Splash';
+import MapComponent from './Components/map';
+import NoticeComponent from './Components/notice';
+import SearchBarComponent from './Components/searchbar';
+import {useState, useEffect} from 'react';
+import {enableScreens} from 'react-native-screens';
+import {RecoilRoot} from 'recoil';
+import SplashScreen from 'react-native-splash-screen';
+import SideBarComponent from './Components/sidebar';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+
+enableScreens();
+
+/**
+ * navigate 예시
+ */
+{
+  /* <Button onPress={() => navigation.navigate('Modal')} title="open" /> */
+}
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function App() {
+  return (
+    <RecoilRoot>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="home"
+          screenOptions={{
+            headerShown: false,
+          }}>
+          <Stack.Group>
+            <Stack.Screen name="Splash" component={SplashScreens} />
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="open-sidebar" component={DrawerComponent} />
+          </Stack.Group>
+          <Stack.Group screenOptions={{presentation: 'modal'}}>
+            <Stack.Screen name="Modal" component={ModalScreen} />
+          </Stack.Group>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </RecoilRoot>
+  );
+}
 
 function HomeScreen() {
-  const Navigation = useNavigation();
+  const navigation = useNavigation();
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button title="lets go Map" onPress={() => Navigation.navigate('Map')} />
+    <View style={styles.container}>
+      {/* <MapComponent style={styles.kakaoMap} /> */}
+
+      <NoticeComponent />
+      <SearchBarComponent />
+      {/* <ARCameraBtn
+         onPress={() => navigation.navigate('Modal')}
+         title="AR launch"
+       /> */}
+      <TouchableOpacity
+        style={styles.ARCameraButton}
+        onPress={() => navigation.navigate('Modal')}>
+        <Text style={styles.buttonText}>AR 촬영</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-const Stack = createNativeStackNavigator();
-
-function App() {
+function ModalScreen() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Map" component={MapScreen} />
-        <Stack.Screen name="Next" component={NextScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View>
+      <Text>Modal</Text>
+    </View>
   );
 }
+
+function DrawerComponent() {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        drawerType: 'front',
+        drawerPosition: 'right',
+        headerShown: false,
+      }}>
+      <Drawer.Screen name="sidebar" component={SideBarComponent} />
+    </Drawer.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  kakaoMap: {
+    ...StyleSheet.absoluteFillObject,
+  },
+
+  // overlay: {
+  //   position: 'absolute',
+  //   top: 0,
+  //   left: 0,
+  //   right: 0,
+  //   bottom: 0,
+  //   justifyContent: 'center',
+  //   alignItems: 'center',
+  //   pointerEvents: 'none',
+  // },
+
+  ARCameraButton: {
+    position: 'absolute',
+    bottom: 48,
+    width: '90%',
+    height: '8%',
+    backgroundColor: '#35A48F',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 12,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default App;
