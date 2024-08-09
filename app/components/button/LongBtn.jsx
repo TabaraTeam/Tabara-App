fecc30d30e19a1b2845689beb077883cfb7f64e8
GYUHWAN JEONG <105405834+solp721@users.noreply.github.com>
2024-08-09 21:41:55 +0900

    feat : 긴 버튼 추가

---
+++ app/components/button/LongBtn.jsx
@@ -0,0 +1,34 @@
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import React from 'react';
+import {Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {Button} from 'react-native';
+
+/**
+ * AR 카메라 기능 구현 예정
+ */
+export default function ARCameraBtn({navigation}) {
+  return (
+    <TouchableOpacity style={styles.ARCameraButton}>
+      <Text style={styles.buttonText}>AR 촬영</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  ARCameraButton: {
+    position: 'absolute',
+    bottom: 48,
+    width: '90%',
+    height: '8%',
+    backgroundColor: '#35A48F',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 12,
+    zIndex: 99,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
